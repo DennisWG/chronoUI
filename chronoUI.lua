ec7696da0125d31329b102fd4e13ce5982b6a60f
DennisWG <DennisWG@users.noreply.github.com>
2017-07-15 10:19:48 +0200

    Default profile initialization

--- chronoUI.lua
+++ chronoUI.lua
@@ -11,13 +11,15 @@ SLASH_RELOAD1 = "/rl";
     
     Core.Modules = {};
     
-    function Core:Print(...)
+    local function Print(...)
         local msg = "";
-        for i=1, select("#", ...) do
+        for i=2, select("#", ...) do
             msg = msg..tostring(select(i, ...)).."    ";
         end
         DEFAULT_CHAT_FRAME:AddMessage(msg);
     end
+    
+    Core.Print = Print;
     
     Core:SetScript("OnEvent", function(self, event, ...)
         if self[event] then
@@ -28,12 +30,33 @@ SLASH_RELOAD1 = "/rl";
     Core:RegisterEvent("ADDON_LOADED");
 end
 
+-- Initializes the default profile if it hasn't been
+function Core:InitializeProfile()
+    chronoUI_profiles = chronoUI_profiles or
+    {
+        default =
+        {
+            
+        }
+    };
+    
+    for name, module in pairs(self.Modules) do
+        local profile = module:InitializeDefaultProfile();
+        chronoUI_profiles.default[name] = profile;
+    end
+    
+    chronoUI_profiles.current = "default";
+    
+    self.db = chronoUI_profiles;
+end
+
 function Core:ADDON_LOADED(name)
     if name ~= "chronoUI" then
         return;
     end
 
     self:Print("chronoUI loaded!");
+    self:InitializeProfile();
 end
 
 -- Registers a module by the given name
@@ -51,6 +74,9 @@ function Core:RegisterModule(name)
             module[event](self, ...);
         end
     end);
+    
+    module.Print = Core.Print;
+    
     self.Modules[name] = module;
     return module;
 end
